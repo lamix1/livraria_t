fd8e48481a31937a6b8910f135fd3273d54a661a
lamix1 <camilacristina328santoss@gmail.com>
2023-04-04 14:25:28 -0300

    Criando o modelo autor

--- livraria/admin.py
+++ livraria/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Categoria
 from .models import Editora
+from .models import Autor
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
