0b39aaacc7d5753d20f014237fbfef42ac3422bc
lamix1 <camilacristina328santoss@gmail.com>
2023-04-04 14:26:43 -0300

    Criando o modelo livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Categoria
 from .models import Editora
 from .models import Autor
+from .models import Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
+admin.site.register(Livro)
