309424cd9b27870db4cdb97ee14c3e51a035efe5
lamix1 <camilacristina328santoss@gmail.com>
2023-04-04 14:33:07 -0300

    Corrigindo os arquivos admin.py e models.oy

--- livraria/models.py
+++ livraria/models.py
@@ -28,6 +28,10 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0,  null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
