98b85fb515806218e52ea82c3cbee5d0af9e5545
lamix1 <camilacristina328santoss@gmail.com>
2023-04-04 14:43:45 -0300

    Editando o admin.py e o models.py

--- livraria/models.py
+++ livraria/models.py
@@ -4,8 +4,8 @@ class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao
-    
+        return f"{self.descricao} ({self.id})"
+
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
@@ -21,7 +21,8 @@ class Autor(models.Model):
         return self.nome
 
     class Meta:
-        verbose_name_plural = "Autores"
+        verbose_name_plural = "Autores"    
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -32,6 +33,8 @@ class Livro(models.Model):
         Categoria, on_delete=models.PROTECT, related_name="livros"
     )
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
-    
+
+
+
     def __str__(self):
-        return f"{self.titulo} ({self.quantidade})"
+        return f"{self.titulo} ({self.quantidade})"      
